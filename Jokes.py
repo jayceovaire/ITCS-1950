417df436dda85d7fd588cfe2dc756c144c68203a
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2023-05-03 19:44:59 -0400

    Chapter 5 - Jokes Chapter 6 - Dragon Realms

---
+++ Jokes.py
@@ -0,0 +1,12 @@
+
+def jokes():
+    print('Knock Knock')
+    input()
+    print("Who's there?")
+    input()
+    print("interrupting cow.")
+    input()
+    print("Interrupting cow wh", end="")
+    print('-MOO!')
+
+jokes()
